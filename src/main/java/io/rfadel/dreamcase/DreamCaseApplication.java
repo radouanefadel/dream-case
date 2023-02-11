43a05300b334ec86ed257e55b13d2ed21055a853
Radouane EL FADEL <raadouane.ai@gmail.com>
2023-02-11 21:23:29 +0100

    :tada: chore: Init project
    close #1

---
+++ src/main/java/io/rfadel/dreamcase/DreamCaseApplication.java
@@ -0,0 +1,13 @@
+package io.rfadel.dreamcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamCaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/rfadel/dreamcase/DreamCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rfadel.dreamcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
