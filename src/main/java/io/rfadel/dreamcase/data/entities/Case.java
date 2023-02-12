24c9ec8da3588bf4b9715fc1b721f30b4f7672fe
Radouane EL FADEL <raadouane.ai@gmail.com>
2023-02-12 22:27:41 +0100

    :zap: data: Connect to MySQL database and create its tables
    close #2

---
+++ src/main/java/io/rfadel/dreamcase/data/entities/Case.java
@@ -0,0 +1,35 @@
+package io.rfadel.dreamcase.data.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "case")
+@Getter
+@Setter
+public class Case {
+    @Id()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long caseId;
+    @Column(unique = true, nullable = false)
+    private String title;
+
+    @Column(nullable = true)
+    private String description;
+
+    @Column(name = "creation_date", nullable = false)
+    private LocalDateTime creationDate;
+
+    @Column(name = "last_update_date", nullable = true)
+    private LocalDateTime lastUpdateDate;
+
+    @PrePersist
+    public void prePersist() {
+        if (this.creationDate == null) {
+            this.creationDate = LocalDateTime.now();
+        }
+    }
+}
