5da38e4750d1189a8ebad2225927e866a31826b7
Radouane EL FADEL <raadouane.ai@gmail.com>
2023-02-13 01:02:57 +0100

    :recycle: refactor: Update CreateOrUpdateCaseDto

--- src/main/java/io/rfadel/dreamcase/data/dtos/CreateOrUpdateCaseDto.java
+++ src/main/java/io/rfadel/dreamcase/data/dtos/CreateOrUpdateCaseDto.java
@@ -3,7 +3,6 @@ package io.rfadel.dreamcase.data.dtos;
 import javax.validation.constraints.NotNull;
 
 public class CreateOrUpdateCaseDto {
-    private long case_id;
     @NotNull
     private String title;
 
